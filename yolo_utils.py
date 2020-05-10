ef6f0af087718b7a84897534d2cbd9902f2da3e9
adityadesaia3 <adityadesaia3@gmail.com>
2020-05-10 21:28:30 +0530

    added minimum distance check to end vehicle coordinate's detection

--- yolo_utils.py
+++ yolo_utils.py
@@ -329,7 +329,7 @@ def draw_labels_and_boxes(img, boxes, confidences, classids, idxs, colors, label
 
                             perpendicular_distance = perpendicular_distance_from_second_line(bottom_mid_point[0], bottom_mid_point[1])
                             print(f"Perpendicular distance: {perpendicular_distance}\tMinimum Distance: {minimum_distance}")
-                            if perpendicular_distance > minimum_distance:
+                            if perpendicular_distance > minimum_distance or minimum_distance > 100:
                                 vehicle_number = None
                         except:
                             print("Unexpected error:", sys.exc_info()[0])
